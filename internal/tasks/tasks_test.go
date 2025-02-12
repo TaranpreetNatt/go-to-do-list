6e87ce29ec78c4154622ea5f2358d6ae87bb06f9
Taranpreet Natt <taranpreetnatt@Mac.lan>
2025-02-11 22:13:10 -0800

    Refactored task_test.go to have table driven tests

--- internal/tasks/tasks_test.go
+++ internal/tasks/tasks_test.go
@@ -8,19 +8,46 @@ import (
 )
 
 func TestCreatingTasks(t *testing.T) {
-	var buf bytes.Buffer
-
-	task := tasks.Task{ID: 1, Task: "task", Done: false}
-	err := tasks.CreateTask(&buf, task)
-
-	if err != nil {
-		t.Fatalf("Error testing creating a task: %v", err)
+	taskTests := []struct {
+		name     string
+		Task     tasks.Task
+		expected string
+	}{
+		{
+			name:     "task with one word",
+			Task:     tasks.Task{ID: 1, Task: "Task", Done: false},
+			expected: "1,Task,false\r\n",
+		},
+		{
+			name:     "task with two words",
+			Task:     tasks.Task{ID: 2, Task: "Task word", Done: false},
+			expected: "2,Task word,false\r\n",
+		},
+		{
+			name:     "Empty task",
+			Task:     tasks.Task{ID: 3, Task: "", Done: true},
+			expected: "3,,true\r\n",
+		},
 	}
 
-	got := buf.String()
-	want := "1,task,false\r\n"
+	for _, tt := range taskTests {
+		t.Run(tt.name, func(t *testing.T) {
+			var buf bytes.Buffer
+			err := tasks.CreateTask(&buf, tt.Task)
+
+			if err != nil {
+				t.Fatalf("Error testing creating a task: %v", err)
+			}
+			assertTask(t, buf.String(), tt.expected)
+		})
+	}
+}
+
+func assertTask(t *testing.T, got, want string) {
+	t.Helper()
 
 	if got != want {
 		t.Errorf("got %v, want %v", got, want)
 	}
+
 }
